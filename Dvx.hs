8a02de111c5979c6dcdd6dcd121bcb599e9bc994
Hamcha <zikyky@gmail.com>
2015-02-24 10:11:34 +0100

    Fix linting errors

--- Dvx.hs
+++ Dvx.hs
@@ -4,17 +4,16 @@ module Main where
 
 import Utils (trim)
 import System.Environment
-import System.IO
 
 tokenize :: [String] -> [[String]]
 tokenize =
     nonempty . map (nonempty . tokenizeLine)
     where
     tokenizeLine = splitAndKeep " ," [] . trim
-    nonempty = filter (\x -> length x > 0)
+    nonempty = filter (not . null)
 
-splitAndKeep :: [Char] -> String -> String -> [String]
-splitAndKeep _ s  []     = s : []
+splitAndKeep :: String -> String -> String -> [String]
+splitAndKeep _ s  []     = [s]
 splitAndKeep c s (x:xs)  =
     case find c x of
         Just a  -> s : [a] : splitAndKeep c []         xs
@@ -22,10 +21,10 @@ splitAndKeep c s (x:xs)  =
     where
     find :: Eq a => [a] -> a -> Maybe a
     find []     _             = Nothing
-    find (c:cx) x | c == x    = Just c
-                  | otherwise = find cx x
-
+    find (i:ix) j | i == j    = Just j
+                  | otherwise = find ix j
+main :: IO ()
 main =
     getArgs >>=
-    \case []     -> getProgName >>= \x -> putStr $ concat ["Usage: ", x, " <file.dvx>\n"]
-          (x:xs) -> print . tokenize . lines =<< readFile x
+    \case []    -> getProgName >>= \x -> putStr $ concat ["Usage: ", x, " <file.dvx>\n"]
+          (x:_) -> print . tokenize . lines =<< readFile x
