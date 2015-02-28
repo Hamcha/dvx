db7535be12a5a5a295f5de2e898e0e50a7adbc27
silverweed <silverweed1991@gmail.com>
2015-02-28 20:38:20 +0100

    allow main to parse stdin

--- dvx.hs
+++ dvx.hs
@@ -4,8 +4,15 @@ module Main where
 
 import Dvx.Parser
 import System.Environment
+import System.IO
 
 main :: IO ()
 main = getArgs >>=
-       \case []    -> getProgName >>= \x -> putStr $ concat ["Usage: ", x, " <file.dvx>\n"]
-             (x:_) -> print . parse . tokenize . lines =<< readFile x
+       \case []      -> getProgName >>= \x -> putStr $ concat ["Usage: ", x, " <file.dvx>\n"]
+             ("-":_) -> parseStdin ""
+             (x:_)   -> print . parse . tokenize . lines =<< readFile x
+
+parseStdin :: String -> IO ()
+parseStdin x = isEOF >>=
+                \case True  -> print . parse . tokenize . lines $ x
+                      False -> getLine >>= \y -> parseStdin $ x ++ y
