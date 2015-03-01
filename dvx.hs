b815fd3a72907b2de24f246cee09bca0066f8471
Hamcha <zikyky@gmail.com>
2015-03-01 03:01:00 +0100

    Cleaner main

--- dvx.hs
+++ dvx.hs
@@ -6,13 +6,15 @@ import Dvx.Parser
 import System.Environment
 import System.IO
 
+execute :: [String] -> IO ()
+execute = print . parse . tokenize
+
 main :: IO ()
 main = getArgs >>=
        \case []      -> getProgName >>= \x -> putStr $ concat ["Usage: ", x, " <file.dvx>\n"]
-             ("-":_) -> parseStdin ""
-             (x:_)   -> print . parse . tokenize . lines =<< readFile x
+             ("-":_) -> isEOF      >>= parseStdin []
+             (x:_)   -> readFile x >>= execute . lines
 
-parseStdin :: String -> IO ()
-parseStdin x = isEOF >>=
-                \case True  -> print . parse . tokenize . lines $ x
-                      False -> getLine >>= \y -> parseStdin $ x ++ y
+parseStdin :: [String] -> Bool -> IO ()
+parseStdin x True  = execute x
+parseStdin x False = getLine >>= \y -> isEOF >>= parseStdin (x ++ [y])
