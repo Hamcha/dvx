f33df470d6a78f9cb10793af187d86ccdf12ad42
Hamcha <zikyky@gmail.com>
2015-03-02 14:32:10 +0100

    Nested expressions executing

--- Dvx/Interpreter.hs
+++ Dvx/Interpreter.hs
@@ -13,8 +13,10 @@ execute c (x:xs) = executeExpr c x >>= \cx -> execute (snd cx) xs
 
 executeExpr :: [Context] -> DvxExpr -> IO (DvxValue, [Context])
 executeExpr c DvxStart          = return (TypeNil, c)
-executeExpr c (DvxDecl name v)  = return $ setVar c name v
-executeExpr c (DvxCall fn args) = apply (getVar c fn) (resolve c args) >>= \x -> return (x, c)
+executeExpr c (DvxDecl name v)  = setVar c name v
+executeExpr c (DvxCall fn args) = resolve c args
+								  >>= \var -> apply (getVar c fn) var
+								  >>= \x -> return (x, c)
 executeExpr _ x                 = error $ "Can't execute expression: " ++ show x
 
 getVar :: [Context] -> String -> DvxValue
@@ -24,20 +26,22 @@ getVar (c:cs) str =
         Just value -> value
         Nothing    -> getVar cs str
 
-setVar :: [Context] -> String -> DvxExpr -> (DvxValue, [Context])
-setVar c str expr = (value, newcontext : tail c)
-                    where
-                    newcontext = (str, value) : head c
-                    value = resolveValue c expr
+setVar :: [Context] -> String -> DvxExpr -> IO (DvxValue, [Context])
+setVar c str expr = resolveValue c expr
+					>>= \value -> return (value, ((str, value) : head c) : tail c)
 
 apply :: DvxValue -> [DvxValue] -> IO DvxValue
 apply (TypeFun f) args = f args
 apply _           _    = return TypeNil
 
-resolve :: [Context] -> [DvxExpr] -> [DvxValue]
-resolve c = map $ resolveValue c
+resolve :: [Context] -> [DvxExpr] -> IO [DvxValue]
+resolve _ []     = return []
+resolve c (x:xs) = resolveValue c x
+				   >>= \val -> resolve c xs
+				   >>= \list -> return $ val:list
 
-resolveValue :: [Context] -> DvxExpr -> DvxValue
-resolveValue _ (DvxConst x) = x
-resolveValue c (DvxVar s)   = getVar c s
-resolveValue _ v            = error $ "Can't resolve " ++ show v
+resolveValue :: [Context] -> DvxExpr -> IO DvxValue
+resolveValue _ (DvxConst x)      = return x
+resolveValue c (DvxVar s)        = return $ getVar c s
+resolveValue c (DvxCall fn args) = return . fst =<< executeExpr c (DvxCall fn args)
+resolveValue _ v                 = error $ "Can't resolve " ++ show v
