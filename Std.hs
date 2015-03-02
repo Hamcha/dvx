8502101ab23015e7284d3d4623638a5f91fb158d
silverweed <silverweed1991@gmail.com>
2015-03-02 14:24:42 +0100

    implemented basic arithmetic operators

--- Std.hs
+++ Std.hs
@@ -8,6 +8,10 @@ import Dvx.Parser
 stdContext :: Context
 stdContext = [("SCRIVO", TypeFun stdPrint)
              ,("ACCAPO", TypeStr "\n")
+             ,("PIV",    TypeFun $ stdArith (+))
+             ,("MENO",   TypeFun $ stdArith (-))
+             ,("PER",    TypeFun $ stdArith (*))
+             ,("DIVISO", TypeFun $ stdArith div)
              ]
 
 stdPrint :: Function
@@ -16,3 +20,11 @@ stdPrint ((TypeStr s):r) = do putStr s;        stdPrint r
 stdPrint ((TypeInt i):r) = do putStr $ show i; stdPrint r
 stdPrint ((TypeNil  ):r) = do putStr "null";   stdPrint r
 stdPrint (x          :r) = do print x;         stdPrint r
+
+stdArith :: Monad m => (Int -> Int -> Int) -> [DvxValue] -> m DvxValue
+stdArith f = return . TypeInt . func
+             where
+             func :: [DvxValue] -> Int
+             func [] = 0
+             func (TypeInt x:xs) = f x (func xs)
+             func _ = error "stdArith: invalid operands"
