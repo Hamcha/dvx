ecce20005fb3885c97e84691b5c379ab7a355e72
Hamcha <zikyky@gmail.com>
2015-03-03 17:17:46 +0100

    Interpreter works

--- Dvx/Interpreter.hs
+++ Dvx/Interpreter.hs
@@ -12,12 +12,17 @@ execute _ []     = return ()
 execute c (x:xs) = executeExpr c x >>= \cx -> execute (snd cx) xs
 
 executeExpr :: [Context] -> DvxExpr -> IO (DvxValue, [Context])
-executeExpr c DvxStart          = return (TypeNil, c)
-executeExpr c (DvxDecl name v)  = setVar c name v
-executeExpr c (DvxCall fn args) = resolve c args
-                                  >>= \var -> apply (getVar c fn) var
-                                  >>= \x -> return (x, c)
+executeExpr c DvxStart                = return (TypeNil, c)
+executeExpr c (DvxDecl name value)    = setVar c name value
+executeExpr c (DvxCall fn args)       = resolve c args
+                                        >>= \var -> apply (getVar c fn) var
+                                        >>= \x -> return (x, c)
+executeExpr c (DvxFunc name arg body) = return (f, appendContext c name f)
+                                        where f = TypeFun $ makeFunction c arg body
 executeExpr _ x                 = error $ "Can't execute expression: " ++ show x
+
+appendContext :: [Context] -> String -> DvxValue -> [Context]
+appendContext c str value = ((str, value) : head c) : tail c
 
 getVar :: [Context] -> String -> DvxValue
 getVar []     str = error $ "Undefined value: " ++ str
@@ -28,7 +33,17 @@ getVar (c:cs) str =
 
 setVar :: [Context] -> String -> DvxExpr -> IO (DvxValue, [Context])
 setVar c str expr = resolveValue c expr
-                    >>= \value -> return (value, ((str, value) : head c) : tail c)
+                    >>= \value -> return (value, appendContext c str value)
+
+makeFunction :: [Context] -> [String] -> DvxExpr -> Function
+makeFunction c args body fargs = return . fst =<< executeExpr (ctx:c) body
+                                 where ctx = bindArgs args fargs
+
+bindArgs :: [String] -> [DvxValue] -> Context
+bindArgs []     []     = []
+bindArgs []     _      = error "Argument count mismatch (too many)"
+bindArgs _      []     = error "Argument count mismatch (too few)"
+bindArgs (a:ax) (v:vx) = (a, v) : bindArgs ax vx
 
 apply :: DvxValue -> [DvxValue] -> IO DvxValue
 apply (TypeFun f) args = f args
