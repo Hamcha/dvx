12ec3bede74150b4f7d3927a93659b322e6ac0ef
Hamcha <zikyky@gmail.com>
2015-03-03 17:26:57 +0100

    Additional trim just in case

--- Dvx/Parser.hs
+++ Dvx/Parser.hs
@@ -52,7 +52,7 @@ makeast (DvxTok TDefn
                           :_)
                  :_)
         :ys)
-        = DvxFunc name (getArgs args) (makeast expr !! 0) : makeast ys
+        = DvxFunc (trim name) (getArgs args) (makeast expr !! 0) : makeast ys
 -- Variable declaration
 makeast (DvxTok TDefVar
         :DvxList (DvxTok value
@@ -61,7 +61,7 @@ makeast (DvxTok TDefVar
                           :_)
                  :_)
         :ys)
-        = DvxDecl name (discover value) : makeast ys
+        = DvxDecl (trim name) (discover value) : makeast ys
 -- Nullcall (È, DI etc.)
 makeast (DvxTok TNullCall  :xs) = makeast xs
 -- Function call
